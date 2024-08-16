20561968862245c1f3a589735ae309d8207d7dc5
yasminheno <yasmin.heno@campus.technion.ac.il>
2024-08-16 21:28:06 +0300

    g

--- MatamStory.cpp
+++ MatamStory.cpp
@@ -184,7 +184,7 @@ bool checkName(string& name)
         return false;
     }
 
-    for (int i = 0 ; i < name.length() ; i++) {
+    for (std::size_t i = 0 ; i < name.length() ; i++) {
         if (!((name[i] >= 'A' && name[i] <= 'Z') || (name[i] >= 'a' && name[i] <= 'z'))) {
             return false;
         }
